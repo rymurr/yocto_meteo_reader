13c7760f40eff4d6697a1fb9a70d03fabeb64873
Ryan Murray <rymurr@gmail.com>
2013-08-04 11:08:36 +0000

    added temperature sensor subclass

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 
+#include "glog/logging.h"
 #include "sensor.hpp"
 
 using namespace std;
@@ -6,5 +7,6 @@ using namespace std;
 
 int main(int argc, const char * argv[])
 {
+    google::InitGoogleLogging(argv[0]);
     return SensorGroup().start();
 }
--- src/sensor.hpp
+++ src/sensor.hpp
@@ -2,6 +2,11 @@
 #ifndef _SENSOR_HPP_
 #define _SENSOR_HPP_
 
+#include <boost/shared_ptr.hpp>
+#include <boost/assign/list_of.hpp>
+#include <map>
+#include <string>
+
 #include "yocto_api.h"
 #include "yocto_temperature.h"
 #include "yocto_humidity.h"
@@ -28,56 +33,80 @@ static void lightSensorChangeCallBack(YPressure *fct, const string& value)
     LOG(INFO) << "Pressure change       :" << fct->describe() << " = " << value << "lx";
 }
 
+class Sensor {
+    public:
+        virtual void start() = 0;
 
-static void deviceArrival(YModule *m)
-{
-    LOG(INFO) << "Device arrival          : " << m->describe() ;
-    int fctcount = m->functionCount();
-    string fctName, fctFullName;
+};
 
-    for (int i = 0; i < fctcount; i++) {
-        fctName = m->functionId(i);
-        fctFullName = m->get_serialNumber() + "." + fctName;
+class TemperatureSensor: Sensor {
+    private:
+        boost::shared_ptr<YTemperature> _sensor;
+        const std::string _device, _function, _fullName;
 
-        // register call back for anbuttons
-        if (fctName.find("humidity")==0) { 
-            YHumidity *t = YHumidity::FindHumidity(fctFullName);
-            t->registerValueCallback(anButtonChangeCallBack);
-            LOG(INFO) << "Callback registered for : " << fctFullName;
+        static void _callback(YTemperature *fct, const std::string& value) {
+            LOG(INFO) << "Temp: " << fct->describe() << " = " << value;
         }
 
-        // register call back for temperature sensors
-        if (fctName.find("temperature")==0) { 
-            YTemperature *t = YTemperature::FindTemperature(fctFullName);
-            t->registerValueCallback(temperatureChangeCallBack);
-            LOG(INFO) << "Callback registered for : " << fctFullName ;
+    public:
+        TemperatureSensor(const std::string& device, const std::string& function): _device(device), _function(function), _fullName(device + "." + function) {
         }
 
-        // register call back for light sensors
-        if (fctName.find("pressure")==0) { 
-            YPressure *l = YPressure::FindPressure(fctFullName);
-            l->registerValueCallback(lightSensorChangeCallBack);
-            LOG(INFO) << "Callback registered for : " << fctFullName;
+        virtual void start() {
+           _sensor = boost::shared_ptr<YTemperature>(YTemperature::FindTemperature(_fullName));
+           _sensor -> registerValueCallback(this->_callback);
+           LOG(INFO) << "Callback registered for: " << _fullName ;
         }
-        // and so on for other sensor type.....
-    }
-}
-
-static void deviceRemoval(YModule *m)
-{
-  LOG(INFO) << "Device removal: " << m->get_serialNumber();
-}
-
+};
 class SensorGroup { 
+
+    private:
+        std::map<std::string, int> _sensors;
+        static void _deviceRemoval(YModule *m) {
+            LOG(INFO) << "Devince removal: " << m->get_serialNumber();
+        }
+
+        static void _deviceArrival(YModule *m) {
+            LOG(INFO) << "Device arrival: " << m->describe() ;
+            int fctcount = m->functionCount();
+            std::string fctName, fctFullName;
+            TemperatureSensor x;
+        
+            for (int i = 0; i < fctcount; i++) {
+                fctName = m->functionId(i);
+                fctFullName = m->get_serialNumber() + "." + fctName;
+
+                // register call back for anbuttons
+                if (fctName.find("humidity")==0) { 
+                    YHumidity *t = YHumidity::FindHumidity(fctFullName);
+                    t->registerValueCallback(anButtonChangeCallBack);
+                    LOG(INFO) << "Callback registered for : " << fctFullName;
+                }
+
+                // register call back for temperature sensors
+                if (fctName.find("temperature")==0) { 
+                    x = TemperatureSensor(m->get_serialNumber(), fctName);
+                    x.start();
+                }
+
+                // register call back for light sensors
+                if (fctName.find("pressure")==0) { 
+                    YPressure *l = YPressure::FindPressure(fctFullName);
+                    l->registerValueCallback(lightSensorChangeCallBack);
+                    LOG(INFO) << "Callback registered for : " << fctFullName;
+                }
+                // and so on for other sensor type.....
+            }
+        }
 
     public:
         SensorGroup() {
             YAPI::RegisterLogFunction(log);
-            YAPI::RegisterDeviceArrivalCallback(deviceArrival);
-            YAPI::RegisterDeviceRemovalCallback(deviceRemoval);
+            YAPI::RegisterDeviceArrivalCallback(this->_deviceArrival);
+            YAPI::RegisterDeviceRemovalCallback(this->_deviceRemoval);
             YAPI::DisableExceptions();
     
-
+            _sensors = boost::assign::map_list_of("temperature", 1)("humidity", 2)("pressure", 3);
         }
 
         int start() {
