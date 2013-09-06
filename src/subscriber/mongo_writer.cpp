1a41dd6d537b794c664ec95437488210c8dcb0e0
Ryan Murray <rymurr@gmail.com>
2013-09-06 12:57:15 +0100

    small change for compilatio on arm

--- src/subscriber/mongo_writer.cpp
+++ src/subscriber/mongo_writer.cpp
@@ -8,15 +8,14 @@
 #include "mongo_message.hpp"
 #endif
 
+#ifdef MONGO_AVAIL
 int MongoWriter::drain(msgArr msgs) {
-    #ifdef MONGO_AVAIL
     mongo::DBClientConnection c;
     c.connect(_hostname);
     for (std::size_t i=0;i<msgs.size();++i) {
         BOOST_LOG_TRIVIAL(info) << "inserting " << msgs[i]->string();
         c.insert("meteo.measurement", boost::static_pointer_cast<MongoMessage>(convert(msgs[i],_msg_type,BSON))->obj());
     }
-    #endif
     return 1;
 }
-
+#endif
